47875037b8022baa654e18012bd495962c307045
oemes22 <denis-major@web.de>
2026-03-29 14:52:08 +0200

    V1.6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'lnr-app-v1.5';
-const VERSION = '1.5 Schinkenstulle'; // Diese Variable wird an die Webseite gesendet
+const CACHE_NAME = 'lnr-app-v1.6';
+const VERSION = '1.6 Schinkenstulle'; // Diese Variable wird an die Webseite gesendet
 const bwChannel = new BroadcastChannel('sw_status');
 
 const ASSETS = [
