0bda8b5e82923ad100f7fc2a1f66f552f9b37365
oemes22 <denis-major@web.de>
2026-04-02 19:40:14 +0200

    V1.2
    Änderungen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'lnr-app-v1.1';
-const VERSION = '1.1 Butterschnitte'; // Diese Variable wird an die Webseite gesendet
+const CACHE_NAME = 'lnr-app-v1.2';
+const VERSION = '1.2 Bratwurst'; // Diese Variable wird an die Webseite gesendet
 const bwChannel = new BroadcastChannel('sw_status');
 
 const ASSETS = [
