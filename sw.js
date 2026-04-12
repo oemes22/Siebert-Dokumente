81912005fec44e1a303c97e05371f0cae4d11fff
oemes22 <denis-major@web.de>
2026-04-12 16:59:34 +0200

    V2.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'lnr-app-v2.1';
-const VERSION = '2.1 Schnitzel'; // Diese Variable wird an die Webseite gesendet
+const CACHE_NAME = 'lnr-app-v2.2';
+const VERSION = '2.2 Schnitzel'; // Diese Variable wird an die Webseite gesendet
 const bwChannel = new BroadcastChannel('sw_status');
 
 const ASSETS = [
